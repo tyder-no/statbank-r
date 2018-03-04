f754a4fae7d31c69ea80c2233f66456f213da462
t_a_ydersbond <t_a_ydersbond@yahoo.no>
2018-03-04 19:48:48 +0100

    Expanded mortality table example

--- ssb_mortality_table_testing.R
+++ ssb_mortality_table_testing.R
@@ -78,16 +78,71 @@ testMetaDataAndData07902 <- function(){
     list(metaData=metaData,tableData=tableData)
 }
 
+#
+# Example of "manual" setup for data extraction 
+#
+
+pickMortalityYears <- function(mdVL,yearsPicked=c(47,48,49,50,51)){
+    mdVL$Kjonn[1,3] <- 10 ; # All
+    mdVL$AlderX[1,3] <- 10 ; # All
+    mdVL$ContentsCode[1,3] <- 10 ; # All
+    for (i in 1:length(yearsPicked) mdVL$Tid[yearsPicked[i],3] <- 1 ; # Selected
+    
+    mdVL
+}
+
+#
+# Fetching expected life time data
+#
+
+getMortalityYearsData <- function(yearsPicked){
+    
+    metaData <- getMDValuesLabels07902() ;     
+    mdVL <- pickMortalityYears(metaData,yearsPicked=yearsPicked) ;
+    eQuery <- createSearchFromDF(mdVL) ;
+    eData <- getJSONData("07902",eQuery)
+   # eData$ContentsCode <- NULL # Drop trivial column
+    eData
+}
+
+
+#
+#  Using melt&cast from reshape to transform to columns
+#
+
+transformToColumnsYearsData <- function(eData){
+    eData$expLT <- eData$value # To avoid default naming collision on "value"
+    eData$value <- NULL ;  # Drop value column
+    meltYears <- melt(eData,id=c("Kjonn","AlderX","Tid")) ;
+  #  meltExpectation$expLT <- NULL ;  # Drop trivial column
+    colExp <- cast(meltYears,Kjonn+AlderX~Tid)
+
+    colExp 
+}
+
+
+#
+# Putting it all together - this function will give expected life times in columns
+# Grouped by sex in first column  
+#
+
+getMortalityDataTable <- function(contCode){
+
+    eData <- getMortalityData(contCode)
+    transformToColumnsData(eData)
+
+}
 
 #
 # Example of "manual" setup for data extraction 
 #
 
-pickExpectedLifeTime <- function(mdVL){
+pickMortalityVars <- function(mdVL,contCode=3){
     mdVL$Kjonn[1,3] <- 10 ; # All
     mdVL$AlderX[1,3] <- 10 ; # All
-    mdVL$ContentsCode[3,3] <- 1 ; # Expected life years
+    mdVL$ContentsCode[contCode,3] <- 1 ; # Expected life years
     mdVL$Tid[1,3] <- 10 ; # All
+    
     mdVL
 }
 
@@ -96,10 +151,10 @@ pickExpectedLifeTime <- function(mdVL){
 # Fetching expected life time data
 #
 
-getExpectedLifeTime <- function(){
+getMortalityData <- function(contCode){
     
     metaData <- getMDValuesLabels07902() ;     
-    mdVL <- pickExpectedLifeTime(metaData) ;
+    mdVL <- pickMortalityVars(metaData,contCode=contCode) ;
     eQuery <- createSearchFromDF(mdVL) ;
     eData <- getJSONData("07902",eQuery)
     eData$ContentsCode <- NULL # Drop trivial column
@@ -111,7 +166,7 @@ getExpectedLifeTime <- function(){
 #  Using melt&cast from reshape to transform to columns
 #
 
-transformToColumnsExpectations <- function(eData){
+transformToColumnsData <- function(eData){
     eData$expLT <- eData$value # To avoid default naming collision on "value"
     eData$value <- NULL ;  # Drop value column
     meltExpectation <- melt(eData,id=c("Kjonn","AlderX","Tid")) ;
@@ -127,11 +182,40 @@ transformToColumnsExpectations <- function(eData){
 # Grouped by sex in first column  
 #
 
+getMortalityDataTable <- function(contCode){
+
+    eData <- getMortalityData(contCode)
+    transformToColumnsData(eData)
+
+}
+
+
+
+getSurvivalTable <- function(){
+
+    getMortalityDataTable(1)
+  
+}
+
+getDeathsTable <- function(){
+
+    getMortalityDataTable(2)
+  
+}
+
+
+
 getExpectedLifeTimeTable <- function(){
 
-    eData <- getExpectedLifeTime()
-    transformToColumnsExpectations(eData)
+    getMortalityDataTable(3)
+  
+}
 
+
+getProbabilityOfDeathTable <- function(){
+
+    getMortalityDataTable(4)
+  
 }
 
 
