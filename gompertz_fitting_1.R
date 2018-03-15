7c392475e2ba7e263c153888390f0c53632fd9d2
t_a_ydersbond <t_a_ydersbond@yahoo.no>
2018-03-15 01:43:21 +0100

    More precise regression, more plotting

--- gompertz_fitting_1.R
+++ gompertz_fitting_1.R
@@ -2,7 +2,8 @@
 #
 # source("gompertz_fitting_1.R")
 #
-                                        #library(ggplot)
+#library(ggplot)
+
 library(dplyr)
 library(ggplot2)
 library(flexsurv)
@@ -107,10 +108,6 @@ paramEstimYear <- function(dF,mkPlot=0,yr=1967,regrStart=50) {
     
     yrM <- data.frame(dF$age,dF$maleYear) ;  yrF <- data.frame(dF$age,dF$femYear) ;
     names(yrM) <- c("age","lx") ;  names(yrF) <- c("age","lx") ;
-   # if (mkPlot==1) {
-   #     X11(height=10,width=12)
-   #     par(mfrow=c(2,2))
-   # }
     yrHM <- prepareHazard(yrM,plotcol=4,yr,regrStart,plotNum=1) ;
     yrHF <- prepareHazard(yrF,plotcol=2,yr,regrStart,plotNum=2) ;
     
@@ -136,16 +133,17 @@ paramEstimSeriesOfYears <- function(sT,years=1967:2017,mkPlot=0,regrStart=50) {
 
 
 
-plotMortalityParameters <- function(resM) {
+plotMortalityParameters <- function(resM,regrStart=50,lastPerStart=2007) {
 
-    yrs <- resM[,1] 
-    aFitM <- lm(resM[,2]~resM[,1]) ;    bFitM <- lm(resM[,3]~resM[,1]) ;
-    aFitF <- lm(resM[,4]~resM[,1]) ;    bFitF <- lm(resM[,5]~resM[,1])
+    yrs <- resM[,1] ; a0M <- resM[,2] - resM[,3]*regrStart ;  a0F <- resM[,4] - resM[,5]*regrStart ;
+    aFitM <- lm(a0M~yrs) ;    bFitM <- lm(resM[,3]~yrs) ;
+    aFitF <- lm(a0F~yrs) ;    bFitF <- lm(resM[,5]~yrs)
     aCoefM <- aFitM$coef ; bCoefM <- bFitM$coef ;
     aCoefF <- aFitF$coef ; bCoefF <- bFitF$coef ;
 
-    aFitM25<- lm(resM[yrs>1991,2]~resM[yrs>1991,1]) ;    bFitM25 <- lm(resM[yrs>1991,3]~resM[yrs>1991,1]) ;
-    aFitF25 <- lm(resM[yrs>1991,4]~resM[yrs>1991,1]) ;    bFitF25 <- lm(resM[yrs>1991,5]~resM[yrs>1991,1])
+    
+    aFitM25<- lm(a0M[yrs>lastPerStart]~resM[yrs>lastPerStart,1]) ;    bFitM25 <- lm(resM[yrs>lastPerStart,3]~resM[yrs>lastPerStart,1]) ;
+    aFitF25 <- lm(a0F[yrs>lastPerStart]~resM[yrs>lastPerStart,1]) ;    bFitF25 <- lm(resM[yrs>lastPerStart,5]~resM[yrs>lastPerStart,1])
     aCoefM25 <- aFitM25$coef ; bCoefM25 <- bFitM25$coef ;
     aCoefF25 <- aFitF25$coef ; bCoefF25 <- bFitF25$coef ;    
 
@@ -153,8 +151,8 @@ plotMortalityParameters <- function(resM) {
     X11(width=12,height=7)
     par(mfrow=c(1,2))
     #png(filename='gomp_f1.png') ;
-    plot(resM[,1],resM[,2],col=4,ylim=c(-7,-4.5),xlab="Year",ylab="Basic mortality",main="Parameter a in y=a+bx")
-    points(resM[,1],resM[,4],col=2)
+    plot(yrs,a0M,col=4,ylim=c(-12.5,-9.5),xlab="Year",ylab="Basic mortality",main="Parameter a in y=a+bx")
+    points(yrs,a0F,col=2)
     abline(aCoefM[1],aCoefM[2],col=4)
     abline(aCoefF[1],aCoefF[2],col=2)
     abline(aCoefM25[1],aCoefM25[2],col=4,lty=3)
@@ -201,20 +199,110 @@ processingGompertz <- function(mkPlotSlct=1,regrStart=50) {
 extraPolDraw <- function(rC,yr,N=100000,regrStart=50)  {
 
     aM <- rC[1] + rC[2]*yr ;  bM <- rC[5] + rC[6]*yr ;
-    rgM <-  regrStart + rgompertz(N,bM,exp(aM))
+    rgM <- rgompertz(N,bM,exp(aM))
     aF <- rC[3] + rC[4]*yr ;  bF <- rC[7] + rC[8]*yr ;
-    rgF <- regrStart + rgompertz(N,bF,exp(aF))
-    list(parm=c(aM,bM,aF,bF), rgM=rgM,rgF=rgF)
+    rgF <- rgompertz(N,bF,exp(aF))
+ 
+    EM0 <- mean(rgM) ; EF0 <- mean(rgF) ; 
+    EM62 <- mean(rgM[rgM>62]) ;  EM67 <- mean(rgM[rgM>67]) ;
+    EF62 <- mean(rgF[rgF>62]) ;  EF67 <- mean(rgF[rgF>67]) ;
+   
+    EM80 <- mean(rgM[rgM>80]) ;  EM90 <- mean(rgM[rgM>90]) ;
+    EF80 <- mean(rgF[rgF>80]) ;  EF90 <- mean(rgF[rgF>90]) ;
+    
+    list(parm=c(aM,bM,aF,bF), rgM=rgM,rgF=rgF,expects=c(EM0,EM62,EM67,EM80,EM90,EF0,EF62,EF67,EF80,EF90))
+}
+
+parmBasedDraw <- function(resM,yr,N=100000,regrStart=50,fYr=1967)  {
+    
+    aM <- resM[yr-fYr+1,2] ;  bM <- resM[yr-fYr+1,3] ;
+    rgM <- rgompertz(N,bM,exp(aM-regrStart*bM))
+    aF <- resM[yr-fYr+1,4]  ;  bF <- resM[yr-fYr+1,5] ;
+    rgF <- rgompertz(N,bF,exp(aF-regrStart*bF))
+
+    EM0 <- mean(rgM) ; EF0 <- mean(rgF) ; 
+    EM62 <- mean(rgM[rgM>62]) ;  EM67 <- mean(rgM[rgM>67]) ;
+    EF62 <- mean(rgF[rgF>62]) ;  EF67 <- mean(rgF[rgF>67]) ;
+   
+    EM80 <- mean(rgM[rgM>80]) ;  EM90 <- mean(rgM[rgM>90]) ;
+    EF80 <- mean(rgF[rgF>80]) ;  EF90 <- mean(rgF[rgF>90]) ;
+    
+    list(parm=c(aM,bM,aF,bF), rgM=rgM,rgF=rgF,expects=c(EM0,EM62,EM67,EM80,EM90,EF0,EF62,EF67,EF80,EF90))
     
 }
 
 
-#> surv2040 <- extraPolDraw( rC,2040)
+plotPopHistograms <- function(rC) {
+
+    histPlotM <- function(sYr,year     ){
+         mText <- paste("Males",year, "Expect 0/67/90: ")
+         mainTitle <- paste(mText,round(sYr$expects[1],1),round(sYr$expects[3],1),round(sYr$expects[5],1),sep=" ") ;
+         hist(sYr$rgM,breaks=50,col=4,main=mainTitle,ylab="Deaths",xlab="Age",ylim=c(0,9000)) ;
+    }
+
+    histPlotF <- function(sYr,year     ){
+         mText <- paste("Females",year, "Expect 0/67/90: ")
+         mainTitle <- paste(mText,round(sYr$expects[6],1),round(sYr$expects[8],1),round(sYr$expects[10],1),sep=" ") ;
+         hist(sYr$rgM,breaks=50,col=2,main=mainTitle,ylab="Deaths",xlab="Age",ylim=c(0,9000)) ;
+    }
+    
+    X11(height=8,width=12); par(mfrow=c(2,3)) ;
+    surv2017 <- extraPolDraw( rC,2017) ;  surv2040 <- extraPolDraw( rC,2040) ;  surv2060 <- extraPolDraw( rC,2060) ;
+    histPlotM(surv2017,2017)  ;   histPlotM(surv2040,2040)  ; histPlotM(surv2060,2060)  ;
+    histPlotF(surv2017,2017)  ;   histPlotF(surv2040,2040)  ; histPlotF(surv2060,2060)  ;
+     
+}
+
+plotHistHistograms <- function(resM) {
+
+    histPlotM <- function(sYr,year     ){
+         mText <- paste("Males",year, "Expect 0/67/90: ")
+         mainTitle <- paste(mText,round(sYr$expects[1],1),round(sYr$expects[3],1),round(sYr$expects[5],1),sep=" ") ;
+         hist(sYr$rgM,breaks=50,col=4,main=mainTitle,ylab="Deaths",xlab="Age",ylim=c(0,8000)) ;
+    }
+
+    histPlotF <- function(sYr,year     ){
+         mText <- paste("Females",year, "Expect 0/67/90: ")
+         mainTitle <- paste(mText,round(sYr$expects[6],1),round(sYr$expects[8],1),round(sYr$expects[10],1),sep=" ") ;
+         hist(sYr$rgM,breaks=50,col=2,main=mainTitle,ylab="Deaths",xlab="Age",ylim=c(0,8000)) ;
+    }
+    
+    X11(height=8,width=12); par(mfrow=c(2,3)) ;
+    surv2017 <- parmBasedDraw( resM,2017) ;   surv1997 <- parmBasedDraw( resM,1997) ;  surv1977 <- parmBasedDraw( resM,1977) ;  
+    histPlotM(surv1977,1977)  ; histPlotM(surv1997,1997)  ; histPlotM(surv2017,2017)  ;  
+    histPlotF(surv1977,1977)  ; histPlotF(surv1997,1997)  ; histPlotF(surv2017,2017)  ;  
+  
+     
+}
+
+
+
+#> pG <- processingGompertz()
+#> pG <- processingGompertz()
+#> rC <- pG$regrC
+#> surv2017 <- extraPolDraw( rC,2017) ; surv2040 <- extraPolDraw( rC,2040) ; surv2060 <- extraPolDraw( rC,2060) ;
+#> par(mfrow=c(1,3))
+#> hist(surv2017$rgM,breaks=50)
+#> hist(surv2040$rgM,breaks=50)
+#> hist(surv2060$rgM,breaks=50)
+#> surv2017 <- extraPolDraw( rC,2017)
+#>  c( mean(surv2017$rgM), mean(surv2017$rgF))
+#[1] 82.30068 85.74297  vs expected 80.9  84.3
 #> c(mean(surv2040$rgM), mean(surv2040$rgF))
 #[1] 87.09236 89.24088
 #> surv2060 <- extraPolDraw( rC,2060)
 #> c( mean(surv2060$rgM), mean(surv2060$rgF))
 #[1] 91.11062 92.34628
-#> surv2017 <- extraPolDraw( rC,2017)
-#>  c( mean(surv2017$rgM), mean(surv2017$rgF))
-#[1] 82.30068 85.74297  vs expected 80.9  84.3
+#> pb2017 <- parmBasedDraw(resM,2017,N=100000,regrStart=50,fYr=1967)
+#> pb2017$expects
+#[1] 88.82454 94.35750 90.63094 95.41299
+#> pb1997 <- parmBasedDraw(resM,1997,N=100000,regrStart=50,fYr=1967)
+#> pb1997$expects
+#[1] 86.55767 93.09283 89.10194 94.60010
+ 
+# pb1997 <- parmBasedDraw(resM,1997,N=100000,regrStart=50,fYr=1967)
+#> pb1997$expects-62
+#[1] 17.78744 19.07724 24.61017 31.04673 21.90984 22.82995 27.05983 32.55004
+#> pb2017 <- parmBasedDraw(resM,2017,N=100000,regrStart=50,fYr=1967)
+#> pb2017$expects-62
+#[1] 21.70059 22.63029 26.84820 32.37957 24.52203 25.22189 28.61228 33.43216
